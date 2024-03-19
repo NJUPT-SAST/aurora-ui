70ac01bc115f82bcfac22bcaa19708a22be2494f
BQX <b22050106@njupt.edu.cn>
2024-03-19 22:10:00 +0800

    feat: optimization #1

--- lib/components/Button/index.ts
+++ lib/components/Button/index.ts
@@ -1,4 +1,4 @@
-import { CSSResultGroup, LitElement, html } from "lit";
+import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { createComponent } from "@lit/react";
 import { classMap } from "lit/directives/class-map.js";
@@ -7,20 +7,20 @@ import styles from "./index.scss?inline";
 
 /**
  * An example element.
- *
  * @slot - This element has a slot
  * @csspart button - The button
  */
 
+// interface ButtonSize {} "small" | "medium" | "large";
+
 @customElement("s-button")
 export class Sbutton extends LitElement {
-  static styles = styles as unknown as CSSResultGroup;
+  static styles = styles;
   @property({ type: String }) color = "primary";
-  @property({ type: String }) size = "medium";
+  @property({ type: String }) size: "small" | "medium" | "large" = "medium";
   @property({ type: String }) shadow = "none";
   @property({ type: Boolean }) disabled = false;
   @property({ type: Boolean }) disabledShadow = true;
-  @property() _onclick: () => void = () => {};
 
   protected render() {
     return html`
@@ -30,7 +30,6 @@ export class Sbutton extends LitElement {
           disabled: this.disabled,
           disabledShadow: this.disabledShadow,
         })}`}
-        @click=${this._onclick}
       >
         <slot></slot>
       </button>
--- lib/vite-env.d.ts
+++ lib/vite-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+/// <reference path="./typings/scss.d.ts" />
