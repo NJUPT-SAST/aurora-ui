8a9e0af769839c14e7a63cea385ce3eed94d5eb9
MaxtuneLee <max@xox.im>
2024-03-21 01:39:48 +0800

    feat: universal examples

--- packages/ui-universal/lib/components/Button/index.ts
+++ packages/ui-universal/lib/components/Button/index.ts
@@ -5,31 +5,57 @@ import { classMap } from "lit/directives/class-map.js";
 import React from "react";
 import styles from "./index.scss?inline";
 
+export interface ButtonProps {
+  /**
+   * The color of the button.
+   */
+  color?: "primary" | "secondary" | "ghost" | "danger" | "border";
+  /**
+   * The size of the button.
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * The shadow of the button.
+   */
+  shadow?:
+  | "regular"
+  | "small"
+  | "medium"
+  | "large"
+  | "extraLarge"
+  | "inner"
+  | "none";
+  /**
+   * If `true`, the button will be disabled.
+   */
+  disabled?: boolean;
+  /**
+   * disabledShadow, when the button is disabled ,the shadow is or not
+   */
+  disabledShadow?: boolean;
+}
+
 /**
- * An example element.
- * @slot - This element has a slot
- * @csspart button - The button
+ * @element s-button
  */
-
-// interface ButtonSize {} "small" | "medium" | "large";
-
 @customElement("s-button")
 export class Sbutton extends LitElement {
   static styles = styles;
-  @property({ type: String }) color = "primary";
-  @property({ type: String }) size: "small" | "medium" | "large" = "medium";
-  @property({ type: String }) shadow = "none";
-  @property({ type: Boolean }) disabled = false;
-  @property({ type: Boolean }) disabledShadow = true;
+  @property({ type: String }) color: ButtonProps["color"] = "primary";
+  @property({ type: String }) size: ButtonProps["size"] = "medium";
+  @property({ type: String }) shadow: ButtonProps["shadow"] = "none";
+  @property({ type: Boolean }) disabled: ButtonProps["disabled"] = false;
+  @property({ type: Boolean }) disabledShadow: ButtonProps["disabledShadow"] =
+    true;
 
   protected render() {
     return html`
       <button
         part="button"
         class=${`base ${this.color} ${this.size} ${this.shadow} ${classMap({
-          disabled: this.disabled,
-          disabledShadow: this.disabledShadow,
-        })}`}
+      disabled: this.disabled as boolean,
+      disabledShadow: this.disabledShadow as boolean,
+    })}`}
       >
         <slot></slot>
       </button>
